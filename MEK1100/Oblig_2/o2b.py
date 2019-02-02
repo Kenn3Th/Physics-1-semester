aee6f06a45aa56858e896e7837374fed4f3d0388
Kenneth R. Eikrehagen <45165531+Kenn3Th@users.noreply.github.com>
2019-02-02 11:05:26 +0100

    Update o2b.py

--- MEK1100/Oblig_2/o2b.py
+++ MEK1100/Oblig_2/o2b.py
@@ -1,16 +1,16 @@
 from oblig2 import*
-#Definerer en variabel til konturplott
+#Defines a variable to the contour plot.
 Z = sqrt(u**2+v**2)
 v1 = linspace(500,4500,7)
 v2 = linspace(0,500,7)
-#kontur plott
-subplot(2,1,1) #Luft
+#Contour plot
+subplot(2,1,1) #Air
 c=contourf(x,y,Z,v1)
 colorbar(c)
 plot(xit,yit,'*',color='black')
 title('Luft')
 ylabel('Y-akse')
-subplot(2,1,2) #Vann
+subplot(2,1,2) #Water
 cs=contourf(x,y,Z,v2)
 colorbar(cs)
 plot(xit,yit,'*',color='black')
