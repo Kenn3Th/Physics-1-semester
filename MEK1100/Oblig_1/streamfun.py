985b6bd94302fa089d766851bc6f0625fe473933
Kenneth R. Eikrehagen <45165531+Kenn3Th@users.noreply.github.com>
2019-02-02 10:47:14 +0100

    Update streamfun.py

--- MEK1100/Oblig_1/streamfun.py
+++ MEK1100/Oblig_1/streamfun.py
@@ -1,10 +1,9 @@
 from numpy import linspace, meshgrid, cos, pi
 
 def streamfun(n=20):
-    '''Regner ut et grid og en stromfunksjon'''
-     
+    #Calculate a grid and a streamfunkction
     x=linspace(-0.5*pi,0.5*pi,n)
-    #resultatet er en vektor med n elementer, fra -pi/2 til pi/2
+    # The result is a vector with n elements, from -pi/2 to pi/2
     [X,Y] = meshgrid(x,x)
     psi=cos(X)*cos(Y)
 
