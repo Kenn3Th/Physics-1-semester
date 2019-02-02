30ce3a7f41312815569ebf893d75bcb91a24e6a4
Kenneth R. Eikrehagen <45165531+Kenn3Th@users.noreply.github.com>
2019-02-02 11:09:57 +0100

    Update oblig2d.py

--- MEK1100/Oblig_2/oblig2d.py
+++ MEK1100/Oblig_2/oblig2d.py
@@ -1,15 +1,15 @@
 from oblig2 import*
 
-#divergens funksjon
+#divergence function
 def div(F,G):
     return gradient(F,axis=1)+gradient(G,axis=0)
-#konturplott
+#Contour plot
 verdier = linspace(-600,600,9)
 contourf(x,y,div(u,v),verdier)
 colorbar()
-#skilleflate
+#Seperate flat
 plot(xit,yit,'*',color='black')
-#Rektanglene
+#Rectangles
 def rektangel(xi,yi,xj,yj):
     x1 = x[yi][xi]; x2 = x[yj][xj]
     y1 = y[yi][xi]; y2 = y[yj][xj]
@@ -20,7 +20,7 @@ def rektangel(xi,yi,xj,yj):
 rektangel(34,159,69,169)
 rektangel(34,84,69,99)
 rektangel(34,49,69,59)  
-#Navngir akser +tittel
+#Giving name to the akses and sets a title
 xlabel('X-akse')
 ylabel('Y-akse')
 title('Divergens')
